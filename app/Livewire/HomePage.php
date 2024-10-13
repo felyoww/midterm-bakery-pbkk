e0e9cecb1cc84c1b28582aa393117b8931b45d90
shafa <5025221078@student.its.ac.id>
2024-10-13 23:13:27 +0700

    feat: update home and category page dynamic

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Home Page - Aetherial Bakery')]
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $categories = Category::where('is_active', 1)->get();
+        return view('livewire.home-page', [
+            'categories' => $categories
+        ]);
     }
 }
